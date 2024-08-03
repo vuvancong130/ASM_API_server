026ba2fd77b39c4109850d5aff36e47551177612
michael_cong <congv8714@gmail.com>
2024-08-03 17:04:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const productRoutes = require("./routes/productsRoute");
+const billsRoutes = require("./routes/billsRoute");
+const historysRoutes = require("./routes/historysRoute");
+
+const app = express();
+const port = 3000;
+
+// Kết nối đến MongoDB
+mongoose.connect("mongodb://localhost:27017/ASM_AND103", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
+
+// Middleware
+app.use(bodyParser.json());
+app.use("/api/products", productRoutes);
+app.use("/api/bills", billsRoutes);
+// Sử dụng các routes cho history
+app.use("/api", historysRoutes);
+
+// Bắt đầu server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
